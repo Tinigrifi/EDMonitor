2d30df8d66ccdc8023b292e592c4f82c9dc6d791
Laf Tinigrifi <laf@tinigrifi.com>
2024-05-25 01:43:02 +0200

    1.2.24145.197

--- EDMonitor/Properties/AssemblyInfo.cs
+++ EDMonitor/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.24.145.195")]
-[assembly: AssemblyFileVersion("1.24.145.195")]
+[assembly: AssemblyVersion("1.2.24145.197")]
+[assembly: AssemblyFileVersion("1.2.24145.197")]
 
-[assembly: AssemblyInformationalVersion("1.24.145.195")]
+[assembly: AssemblyInformationalVersion("1.2.24145.197")]
